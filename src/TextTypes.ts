f57f5a95fdd4240a2cd951eb61c2e6632abb7f39
fratilaandreea1 <88401654+adrianlari@users.noreply.github.com>
2022-09-16 19:18:50 +0300

    major update
    - v1 of native auth token decoder + all styles

---
+++ src/TextTypes.ts
@@ -0,0 +1,12 @@
+export enum TextTypes {
+  DOT = "dot",
+  ADDRESS = "address",
+  BODY = "body",
+  BLOCK_HASH = "blockhash",
+  EXTRA_INFO = "extrainfo",
+  HOST = "host",
+  SIGNATURE = "signature",
+  TTL = "ttl",
+}
+
+export default TextTypes;
