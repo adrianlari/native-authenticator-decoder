24bad78e378836078c49a7d89090c444431d0c89
fratilaandreea1 <88401654+adrianlari@users.noreply.github.com>
2022-09-16 19:20:24 +0300

    extracted some important variables into a different file

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const inputFontSize = "25px";
+const explanationFontSize = "18px";
+const fontFamily = "Roboto Mono";
+const width = "100%";
+const colors = {
+  dot: "#000",
+  address: "#d63aff",
+  blockhash: "#00b9f1",
+  extrainfo: "#fb015b",
+  host: "host",
+  signature: "signature",
+  ttl: "ttl",
+};
+
+export { inputFontSize, explanationFontSize, fontFamily, width, colors };
